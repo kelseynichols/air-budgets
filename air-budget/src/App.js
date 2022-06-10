4e9b162ba55fcebe7bf16c916ea4a7c2001f6389
Kelsey Nichols <kelseynichols2@gmail.com>
2022-06-10 16:40:18 -0500

    winner

--- air-budget/src/App.js
+++ air-budget/src/App.js
@@ -1,11 +1,12 @@
 import buddy from './buddy.webp';
 import './App.css';
+import budgets from './AirBudgets.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={buddy} className="App-logo" alt="logo" /> */}
+        <img src={budgets} className="App-logo" alt="logo" />
         <img src={buddy} className="bud" alt="logo" />
       </header>
     </div>
