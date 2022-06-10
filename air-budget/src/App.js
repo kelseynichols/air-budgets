93da7204fc18da3c7a25ef9c32b85d317cd1f4a9
Kelsey Nichols <kelseynichols2@gmail.com>
2022-06-10 16:06:02 -0500

    bud

---
+++ air-budget/src/App.js
@@ -0,0 +1,15 @@
+import buddy from './buddy.webp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={buddy} className="App-logo" alt="logo" /> */}
+        <img src={buddy} className="bud" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
